fc5e6b6ab3de0fd2f03191eeaece5862c1732176
Oleg Malkov <4.malkov@gmail.com>
2021-10-09 16:19:07 +0300

    done

--- blocks/elements/__btn-heart/elements__btn-heart.js
+++
@@ -1,5 +0,0 @@
-const elementBtnHeart = document.querySelector('.elements__btn-heart');
-
-elementBtnHeart.addEventListener( 'click', () => {
-  elementBtnHeart.classList.toggle('elements__btn-heart_active');
-})
